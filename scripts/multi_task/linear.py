049e67390f8e94a9328187860f40b3b21b4396d5
a.juskiewicz <a.juskiewicz@pixel.com.pl>
2025-07-08 14:32:00 +0200

    implementing algorithm 2

--- scripts/multi_task/linear.py
+++ scripts/multi_task/linear.py
@@ -82,17 +82,17 @@ class MultiTaskLinear:
 
     def fit_prior(self, X: np.ndarray, y: np.ndarray):
         try:
-            self.__validate_dataset(X, y)
+            self._validate_dataset(X, y)
         except ValueError as e:
             logger.error(f"Error occurred while validating dataset - {e}")
             return
 
         self.label_mapping = {}
         for i in range(len(y)):
-            y[i], mapping = self.__encode_labels(y[i])
+            y[i], mapping = self._encode_labels(y[i])
             self.label_mapping[i] = mapping
 
-        self.__init_prior(X)
+        self._init_prior(X)
 
         its = 0
         errors = np.empty((max(X.shape), 1))
@@ -102,14 +102,14 @@ class MultiTaskLinear:
             prev_prior = self.prior
 
             for i in range(max(X.shape)):
-                weights[i], errors[i] = self.__fit_model(X[i], y[i], self.regularization)
+                weights[i], errors[i] = self._fit_model(X[i], y[i], self.regularization, prior=self.prior)
 
             temp = self.prior["regularization"]
-            self.__update_prior(weights)
+            self._update_prior(weights)
             self.prior["regularization"] = temp
             its += 1
 
-            is_converged, num = self.__is_convergence(self.prior, prev_prior)
+            is_converged, num = self._is_convergence(self.prior, prev_prior)
             if is_converged:
                 logger.info(f"Iteration {its} converged, error = {num}")
                 break
@@ -125,12 +125,12 @@ class MultiTaskLinear:
 
         out = {"lambda": self.regularization}
         X_original = X
-        y_internal, mapping = self.__encode_labels(y)
-        out["w"], out["loss"] = self.__fit_model(X, y_internal, out["lambda"])
+        y_internal, mapping = self._encode_labels(y)
+        out["w"], out["loss"] = self._fit_model(X, y_internal, out["lambda"], prior=self.prior)
 
         def task_predict(X_test):
             raw_preds = np.sign(X_test.T @ out["w"])
-            return self.__decode_labels(raw_preds, mapping)
+            return self._decode_labels(raw_preds, mapping)
 
         out["predict"] = task_predict
         out["train_acc"] = np.mean(y == out["predict"](X_original))
@@ -142,21 +142,21 @@ class MultiTaskLinear:
 
         if self.label_mapping:
             mapping = list(self.label_mapping.values())[0]
-            return self.__decode_labels(raw_preds, mapping)
+            return self._decode_labels(raw_preds, mapping)
         else:
             return raw_preds
 
-    def __init_prior(self, X: np.ndarray):
+    def _init_prior(self, X: np.ndarray):
         self.prior["regularization"] = self.regularization
         self.prior["mu"] = np.ones((X[0].shape[0], 1))
         self.prior["sigma"] = np.eye(X[0].shape[0])
         self.prior["W"] = np.zeros((X[0].shape[0], max(X.shape)))
 
-    def __update_prior(self, weights: np.ndarray) -> None:
+    def _update_prior(self, weights: np.ndarray) -> None:
         W = np.concatenate(weights, axis=1)
-        self.prior = self.__update_gaussian_prior(W)
+        self.prior = self._update_gaussian_prior(W)
 
-    def __update_gaussian_prior(self, W: np.ndarray) -> dict:
+    def _update_gaussian_prior(self, W: np.ndarray) -> dict:
         new_prior = {}
         new_prior["W"] = W
         new_prior["mu"] = np.mean(W, axis=1, keepdims=True)
@@ -193,7 +193,7 @@ class MultiTaskLinear:
 
         return new_prior
 
-    def __validate_dataset(self, X: np.ndarray, y: np.ndarray):
+    def _validate_dataset(self, X: np.ndarray, y: np.ndarray):
         if max(X.shape) != max(y.shape):
             raise ValueError(f"Unequal data and label arrays: data - {X.shape}, labels - {y.shape}")
         if max(X.shape) <= 1:
@@ -204,22 +204,22 @@ class MultiTaskLinear:
                     f"Number of datapoints and labels differ: : data - {X[i].shape}, labels - {y[i].shape}"
                 )
 
-    def __fit_model(self, X: np.ndarray, y: np.ndarray, regularization: float) -> tuple[np.ndarray, float]:
-        Ax = self.prior["sigma"] @ X
+    def _fit_model(self, X: np.ndarray, y: np.ndarray, regularization: float, prior=None) -> tuple[np.ndarray, float]:
+        Ax = prior["sigma"] @ X
         A = (1 / regularization) * (Ax @ X.T) + np.eye(X.shape[0])
-        b = (1 / regularization) * (Ax @ y) + self.prior["mu"]
+        b = (1 / regularization) * (Ax @ y) + prior["mu"]
         w = np.linalg.solve(A, b)
-        loss = self.__loss(w, X, y)
+        loss = self._loss(w, X, y)
         return w, loss
 
-    def __is_convergence(self, current_prior: dict, prev_prior: dict) -> tuple[bool, float]:
+    def _is_convergence(self, current_prior: dict, prev_prior: dict) -> tuple[bool, float]:
         W = current_prior["W"]
         W_prev = prev_prior["W"]
         norm = np.linalg.norm(W - W_prev, ord="fro")
         converged = norm < self.max_it_var * np.mean(W)
         return converged, norm
 
-    def __loss(self, w, X, y) -> float:
+    def _loss(self, w, X, y) -> float:
         """
         Compute average squared loss for linear predictions.
         """
@@ -227,13 +227,13 @@ class MultiTaskLinear:
         L = np.linalg.norm(residuals, 2) ** 2 / len(y)
         return L
 
-    def __encode_labels(self, y):
+    def _encode_labels(self, y):
         classes = np.unique(y)
         if len(classes) != 2:
             raise ValueError("Only binary classification supported.")
         mapping = {classes[0]: -1, classes[1]: 1}
         return np.vectorize(mapping.get)(y), mapping
 
-    def __decode_labels(self, y_internal, mapping):
+    def _decode_labels(self, y_internal, mapping):
         inv_mapping = {-1: list(mapping.keys())[0], 1: list(mapping.keys())[1]}
         return np.vectorize(inv_mapping.get)(y_internal)
---
+++ scripts/multi_task/linear_data_selection.py
@@ -0,0 +1,119 @@
+from typing import override
+import numpy as np
+from sklearn.base import BaseEstimator, ClassifierMixin
+from eeg_logger import logger
+from scripts.multi_task.linear import MultiTaskLinear
+
+
+class MultiTaskLinearWithSelectionClassifier(BaseEstimator, ClassifierMixin):
+    """
+    Scikit-learn-compatible classifier designed to handle learning from multiple tasks and apply transfer learning to a new one.
+    It wraps around a base model and provides methods to:
+    - Fit a prior model using multiple sessions (fit_sessions)
+    - Adapt the prior to a new task with selecting weight vectors (fit)
+    - Make predictions using either the prior or task-specific model (predict)
+    - Evaluate the accuracy (score)
+    """
+
+    def __init__(
+        self,
+        num_its=100,
+        regularization=0.5,
+        cov_flag="l2",
+        zero_mean=True,
+        max_it_var=0.0001,
+    ):
+        self.base_model = MultiTaskLinearWithDataSelection(
+            num_its=num_its,
+            regularization=regularization,
+            cov_flag=cov_flag,
+            zero_mean=zero_mean,
+            max_it_var=max_it_var,
+        )
+        self.task_model = None
+
+    def fit_sessions(self, X_sessions, y_sessions):
+        self.base_model.fit_prior(X_sessions, y_sessions)
+        return self
+
+    def fit(self, X, y):
+        self.task_model = self.base_model.fit_new_task(X, y)
+        return self
+
+    def predict(self, X):
+        if self.task_model:
+            return self.task_model["predict"](X)
+        else:
+            return self.base_model.prior_predict(X)
+
+    def score(self, X, y):
+        from sklearn.metrics import accuracy_score
+
+        y_p = self.predict(X)
+        return accuracy_score(y, y_p), y_p
+
+
+class MultiTaskLinearWithDataSelection(MultiTaskLinear):
+
+    def __init__(
+        self,
+        num_its: int = 100,
+        regularization: float = 0.5,
+        cov_flag: str = "l2",
+        zero_mean: bool = True,
+        max_it_var: float = 0.0001,
+    ):
+        self.num_its = num_its
+        self.regularization = regularization
+        self.cov_flag = cov_flag
+        self.zero_mean = zero_mean
+        self.max_it_var = max_it_var
+        self.prior = {}
+        self.label_mapping = {}
+
+    @override
+    def fit_new_task(self, X: np.ndarray, y: np.ndarray) -> dict:
+        if X.shape[0] != max(self.prior["mu"].shape):
+            logger.error("Feature dimensionality of the data does not match this model")
+            return {}
+
+        prior = self.prior
+        mu = prior["mu"]
+        sigma = prior["sigma"]
+        W = prior["W"]
+        selected_weights = []
+
+        # Map labels
+        y_internal, mapping = self._encode_labels(y)
+        # Compute ω_n using the current prior
+        w_n, _ = self._fit_model(X, y_internal, self.regularization, prior=prior)
+
+        for i in range(W.shape[1]):
+            w_s = W[:, i : i + 1]
+            if np.linalg.norm(w_s - w_n) <= np.linalg.norm(w_n - mu):
+                selected_weights.append(w_s)
+        logger.info(f"Selected {len(selected_weights)} prior tasks out of {W.shape[1]} for new task adaptation.")
+
+        # Update prior using selected ω_s and ω_n
+        if selected_weights:
+            selected_weights.append(w_n)  # Include ω_n itself
+            new_W = np.concatenate(selected_weights, axis=1)
+            prior = self._update_gaussian_prior(new_W)
+            mu_diff = np.linalg.norm(prior["mu"] - mu)
+            sigma_diff = np.linalg.norm(prior["sigma"] - sigma)
+            logger.info(f"Updated prior. mu new - mu old = {mu_diff:.4f}, sigma new - sigma old = {sigma_diff:.4f}")
+
+        # Re-fit using updated prior
+        w_n, loss = self._fit_model(X, y_internal, self.regularization, prior=prior)
+
+        def task_predict(X_test):
+            raw_preds = np.sign(X_test.T @ w_n)
+            return self._decode_labels(raw_preds, mapping)
+
+        return {
+            "w": w_n,
+            "loss": loss,
+            "predict": task_predict,
+            "train_acc": np.mean(y == task_predict(X)),
+            "lambda": self.regularization,
+        }
